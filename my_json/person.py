7068dc379ede5440ef226d485dc7f36723e775eb
xuguocheng <xuguocheng@icomwell.com>
2017-05-11 18:09:31 +0800

    json

---
+++ my_json/person.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+#	-*- coding: UTF-8 -*-
+
+import json
+
+
+class Person(object):
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+
+if __name__ == '__main__':
+    obj = dict(id=1, name='张三', age=20)
+    str = json.dumps(obj)
+
+    print 'dict序列化'
+    print type(str)
+    print str
+
+    obj = json.loads(str)
+
+    print 'dict反序列化'
+    print type(obj)
+    print obj
+
+    person = Person(1, '张三', 20)
+    str = json.dumps(person, default=lambda x: x.__dict__)
+
+    print 'person序列化'
+    print str
+
+    person = json.loads(str)
+    print 'person序列化'
+    print person
