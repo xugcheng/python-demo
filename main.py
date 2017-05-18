ab62ccdc9c71ceceddf5d007a003caec97742699
xuguocheng <xuguocheng@icomwell.com>
2017-05-18 10:24:47 +0800

    update

--- main.py
+++ main.py
@@ -3,21 +3,50 @@
 
 import context
 import detect_service
+import threading
+import time
 
 logger = context.G_LOGGER
 
 
 def main():
-    try:
-        school_id = 11
-        logger.info('进出判断---start---,schoolId:%d' % school_id)
-        detect_service.seq_check_in_out_by_school(school_id=school_id)
-        logger.info('进出判断---end-----,schoolId:%d' % school_id)
-    except Exception, e:
-        logger.exception(e)
-    finally:
-        pass
+    school_ids = [1, 11]
+    thread_arr = map(create_thread, school_ids)
+    for i in range(len(thread_arr)):
+        thread_arr[i].start()
+    for i in range(len(thread_arr)):
+        thread_arr[i].join()
+
+
+def create_thread(school_id):
+    thread_name = 'io_%d' % school_id
+    t = threading.Thread(target=loop_check_in_out, name=thread_name, args=(school_id,))
+    return t
+
+
+def loop_check_in_out(school_id):
+    while True:
+
+        try:
+            logger.info('**进出判断--start--,schoolId:%d' % school_id)
+            detect_service.seq_check_in_out_by_school(school_id=school_id)
+            logger.info('**进出判断--end----,schoolId:%d' % school_id)
+            time.sleep(1)
+        except Exception, e:
+            logger.exception(e)
+        finally:
+            pass
+
+
+def test():
+    t1 = threading.Thread(target=loop_check_in_out, name='io_1', args=(1,))
+    t2 = threading.Thread(target=loop_check_in_out, name='io_11', args=(11,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 
 if __name__ == '__main__':
     main()
+    # test()
